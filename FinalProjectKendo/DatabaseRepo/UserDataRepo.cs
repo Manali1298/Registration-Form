4c5d23256d62e89032d88c58febaa5014cdf31c2
Manali Gujarathi <54768615+Manali1298@users.noreply.github.com>
2021-09-01 08:48:43 +0530

    indentation changes

--- FinalProjectKendo/DatabaseRepo/UserDataRepo.cs
+++ FinalProjectKendo/DatabaseRepo/UserDataRepo.cs
@@ -103,40 +103,15 @@ namespace FinalProjectKendo.DatabaseRepo
            
             var city = db.allcity.Where(x => x.state_id.Equals(state_id)).ToList();
             return city;
-            //List<cityData> obj=new List<cityData>();
-            //var citydata = (
-            //from city in db.allcity.Where(x => x.state_id.Equals(state_id)).ToList()
-            //select new
-            //{
-            //    city_id = city.city_id,
-            //    city_name = city.city_name
-            //}).ToList();
-
-            //foreach (var cityobj in citydata)
-            //{
-            //    var cityObj = new cityData();
-            //    cityObj.city_id = cityobj.city_id;
-            //    cityObj.city_name = cityObj.city_name;
-            //    obj.Add(cityObj);
-            //}
-            //return obj;
+           
         }
-        //public UserData GetUserDetails(int nid)
-        //{
-        //    UserData news = db.UserInfos.Where(x => x.u_id == nid).FirstOrDefault();
-        //    return news;
-        //}
+       
         //public stateData Get_State(int  state_id)
         //{
         //    String connections = ("Server=localhost;Port=5432;Database=RegisterDB;User Id=postgres;Password=datapostgre");
         //    NpgsqlConnection connection = new NpgsqlConnection();
         //    connection.ConnectionString = connections;
-        //    //SqlCommand com = new SqlCommand("Select * from mg_state ", connection);
-        //    //com.Parameters.AddWithValue("@catid", country_id);
-        //    //SqlDataAdapter da = new SqlDataAdapter(com);
-        //    //DataSet ds = new DataSet();
-        //    //da.Fill(ds);
-        //    //return ds;
+        //    
 
         //    stateData model = new stateData();
         //    string query = string.Format("SELECT state_name FROM public.mg_state where state_id={0}", state_id);
@@ -164,52 +139,9 @@ namespace FinalProjectKendo.DatabaseRepo
         //        connection.Close();
         //    }
         //    return model;
-
-
         //}
 
-        //Get all City
-        public cityData Get_City(int state_id)
-        {
-            String connections = ("Server=localhost;Port=5432;Database=RegisterDB;User Id=postgres;Password=datapostgre");
-            NpgsqlConnection connection = new NpgsqlConnection();
-            connection.ConnectionString = connections;
-            cityData model = new cityData();
-
-            string query = "SELECT city_name FROM public.mg_city where state_id=@state_id";
-            connection.Open();
-            cmd = new NpgsqlCommand(query, connection);
-
-            try
-            {
-                NpgsqlDataReader dr = cmd.ExecuteReader();
-                if (dr.HasRows)
-                {
-                    while (dr.Read())
-                    {
-                        model.state_id = Convert.ToInt32(dr["state_id"]);
-                        model.city_name = dr["city_name"].ToString();
-
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                connection.Close();
-            }
-            return model;
-
-            //SqlCommand com = new SqlCommand("Select * from mg_city where State_id=@state_id", con);
-            //com.Parameters.AddWithValue("@stateid", state_id);
-            //SqlDataAdapter da = new SqlDataAdapter(com);
-            //DataSet ds = new DataSet();
-            //da.Fill(ds);
-            //return ds;
-        }
+        
         public int GetUserId(string u_email)
         {
             String connections = ("Server=localhost;Port=5432;Database=RegisterDB;User Id=postgres;Password=datapostgre");
